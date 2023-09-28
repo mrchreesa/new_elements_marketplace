c35b93167941ff5219512bc9ecb7f67530de814e
Kristian Rahnev <mrchreesa@gmail.com>
2023-09-28 14:16:33 +0100

    ui

--- pages/apply.tsx
+++ pages/apply.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ApplyComponent from "../components/Apply/ApplyComponent";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
-
+const key = process.env.NEXT_PUBLIC_RECAPTCHAV3_SITE_KEY;
 const Apply = () => {
   return (
     <GoogleReCaptchaProvider
-      reCaptchaKey="6LdRQsklAAAAAMODTeLAWfDsQXcrpPoUMZg8kAAt"
+      reCaptchaKey={key || ""}
       scriptProps={{
         async: false,
         defer: false,
